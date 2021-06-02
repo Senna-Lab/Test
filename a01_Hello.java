b71186ad18adf0159e932cca36838b132bc06d12
SENA-PlumLab <8916317@naver.com>
2021-06-02 14:58:51 +0900

    first commit

---
+++ a01_Hello.java
@@ -0,0 +1,34 @@
+package javaexp.a01_basic;
+
+
+public class a01_Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("자바를 드드드드드디어 시작");
+	}
+
+}
+
+
+
+//jdk 15 다운로드, 설치. 
+//cmd에서 java -version 으로 확인
+
+//oracle win64 다운로드.
+//C:\javaexp\workspace 폴더 만들어놓고, oracle 설치파일을 C:\javaexp에 둠
+//oracle은 java and Web developers 로 설치
+//Installation Folder는 C:\javaexp
+//첫 실행에서 Workspace는 C:\javaexp\workspace
+//설치 완료 후 Window-Preference-General-Workspace에서 encoding 설정 URF-8로 설정
+
+//Dynamic Web Project 생성
+//Package (javaexp.a01_basic) 만들고 
+//Class (a01_Hello.java) 생성하면서 main method 체크 함.
+
+//코드 작성 후 ctrl s로 저장
+//우클릭-run as-java application 으로 실행
+// 실행 단축키 ctrl shift f11
+// 디버그 실행 ctrl f11
+// 주석 단축키 ctrl /
+
